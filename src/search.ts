2be4d9189530bc05ac8ae8fd16a1a659f916da64
Guy Harwood <guy@milkchip.com>
2019-07-22 16:05:19 +0100

    add pro link types

---
+++ src/search.ts
@@ -0,0 +1,42 @@
+import * as debug from 'debug';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { createSocket } from 'dgram';
+import { ANNOUNCEMENT_PORT } from './constants';
+import * as keepalive from './packets/keep-alive';
+
+const d = debug('pro-dj-link:search');
+
+export interface BaseDevice {
+    name: string;
+    device: number;
+}
+
+const _devices: BehaviorSubject<BaseDevice[]> = new BehaviorSubject([]);
+
+export const search = () => {
+    const socket = createSocket({
+        type: 'udp4',
+        reuseAddr: true
+    });
+    socket.bind({
+        port: ANNOUNCEMENT_PORT
+    }, () => {
+        socket.setBroadcast(true);
+        socket.setMulticastTTL(128);
+    });
+    socket.on('message', data => {
+        if (keepalive.test(data)) {
+            const {
+                name,
+                device,
+                ip,
+                mac
+            } = keepalive.parse(data);
+            d(`Player ${device} (${name})`, mac, ip);
+        } else {
+            d('50000', data);
+        }
+    });
+};
+
+export const devices: Observable<BaseDevice[]> = _devices.asObservable();
